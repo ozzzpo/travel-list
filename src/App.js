3ee349f89ef1eed69870b59ecfb89da82d478da5
ozzzpo <onehendrix04@gmail.com>
2023-11-11 00:46:13 +0300

    Add local storage state saving

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
-
+const initialItems =
+  localStorage.getItem("items") !== null
+    ? JSON.parse(localStorage.getItem("items"))
+    : [];
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
   return (
     <div className='App'>
       <Logo />
